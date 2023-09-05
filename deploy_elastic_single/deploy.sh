6199cdcd03834e82020a2c5cac0c692bf24fbdfd
HoangVu0702 <68999625+HoangVu0702@users.noreply.github.com>
2023-09-05 13:48:32 +0700

    Update deploy.sh

--- deploy_elastic_single/deploy.sh
+++ deploy_elastic_single/deploy.sh
@@ -3,10 +3,13 @@ sysctl -w vm.max_map_count=262144
 echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 STACK_VERSION="$1"
 ELASTIC_PASSWORD="$2"
+KIBANA_PASSWORD="$3"
 
 # Gán các giá trị vào tệp .env
 echo "STACK_VERSION=$STACK_VERSION" >> .env
-echo "ELASTIC_PASSWORD=$ELASTIC_PASSWORD" >> .env
+echo "ELASTIC_PASSWORD='$ELASTIC_PASSWORD'" >> .env
+echo "KIBANA_PASSWORD='$KIBANA_PASSWORD'" >> .env
+
 
 docker pull docker.elastic.co/elasticsearch/elasticsearch:"$STACK_VERSION"
 docker pull docker.elastic.co/kibana/kibana:"$STACK_VERSION"
