c63cb40e4f484c7c7b2a95357bf3c388afc57b2f
hvu27 <68999625+hvu27@users.noreply.github.com>
2023-09-07 14:14:08 +0700

    Update install_docker.sh

--- install-docker/install_docker.sh
+++ install-docker/install_docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
-  echo "Script cần chạy với quyền root!"
+  echo "This script requires root privileges!"
   exit 1
 fi
 
 if [ "$1" = "Ubuntu" ]; then
-  echo "Bắt đầu quá trình cài đặt Docker trên Ubuntu..."
+  echo "Starting the Docker installation process on Ubuntu..."
   apt-get update
   apt-get install -y ca-certificates curl gnupg
 
@@ -21,26 +21,28 @@ if [ "$1" = "Ubuntu" ]; then
   apt-get update
   apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  echo "Đã cài đặt Docker trên Ubuntu!"
+  echo "Docker has been installed on Ubuntu!"
 
 elif [ "$1" = "CentOS" ]; then
-  echo "Bắt đầu quá trình cài đặt Docker trên CentOS..."
+  echo "Starting the Docker installation process on CentOS..."
   yum install -y yum-utils
   yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
   yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  echo "Đã cài đặt Docker trên CentOS!"
+  echo "Docker has been installed on CentOS!"
 
 else
-  echo "Hệ điều hành không hỗ trợ hoặc sai tham số!"
-  echo "Sử dụng: sudo bash install_docker.sh Ubuntu hoặc sudo bash install_docker.sh CentOS"
+  echo "Unsupported or incorrect operating system!"
+  echo "Usage: sudo bash install_docker.sh Ubuntu or sudo bash install_docker.sh CentOS"
   exit 1
 fi
-read -p "Bạn có muốn cài đặt Portainer không? (y/n): " choice
+
+read -p "Do you want to install Portainer? (y/n): " choice
 if [ "$choice" = "y" ]; then
   docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-  echo "Đã cài đặt và chạy Portainer!"
+  echo "Portainer has been installed and started!"
 else
-  echo "Không cài đặt Portainer."
+  echo "Portainer will not be installed."
 fi
+
 exit 0
