f2c07eacf5ea829fcba9fc00956c7155c1fa2828
hvu27 <68999625+hvu27@users.noreply.github.com>
2023-09-11 13:56:32 +0700

    Update install_docker.sh

--- install-docker/install_docker.sh
+++ install-docker/install_docker.sh
@@ -38,7 +38,7 @@ else
 fi
 
 read -p "Do you want to install Portainer? (y/n): " choice
-if [ "$choice" = "y" ]; then
+if [ "$choice" = "y" || "$choice" == "Y" ]; then
   docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
   echo "Portainer has been installed and started!"
 else
