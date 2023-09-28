c4972d09f88716259f4eeb2bd69d98fd42c35463
hvu27 <68999625+hvu27@users.noreply.github.com>
2023-09-28 15:32:50 +0700

    Update install_docker.sh

--- install-docker/install_docker.sh
+++ install-docker/install_docker.sh
@@ -5,7 +5,26 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-if [ "$1" = "Ubuntu" ]; then
+# Hàm để xác định hệ điều hành
+detect_os() {
+  if [ -f /etc/os-release ]; then
+    source /etc/os-release
+    if [[ $ID == "ubuntu" || $ID_LIKE == "debian" ]]; then
+      echo "ubuntu"
+    elif [[ $ID == "centos" || $ID == "rhel" || $ID == "fedora" ]]; then
+      echo "centos"
+    else
+      echo "unknown"
+    fi
+  else
+    echo "unknown"
+  fi
+}
+
+# Xác định hệ điều hành
+os=$(detect_os)
+
+if [ "$os" == "ubuntu" ]; then
   echo "Starting the Docker installation process on Ubuntu..."
   apt-get update
   apt-get install -y ca-certificates curl gnupg
@@ -15,15 +34,15 @@ if [ "$1" = "Ubuntu" ]; then
   chmod a+r /etc/apt/keyrings/docker.gpg
 
   echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt-get update
   apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
   echo "Docker has been installed on Ubuntu!"
 
-elif [ "$1" = "CentOS" ]; then
+elif [ "$os" == "centos" ]; then
   echo "Starting the Docker installation process on CentOS..."
   yum install -y yum-utils
   yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -32,16 +51,16 @@ elif [ "$1" = "CentOS" ]; then
   echo "Docker has been installed on CentOS!"
 
 else
-  echo "Unsupported or incorrect operating system!"
-  echo "Usage: sudo bash install_docker.sh Ubuntu or sudo bash install_docker.sh CentOS"
+  echo "Unsupported or unknown operating system!"
   exit 1
 fi
 
 read -p "Do you want to install Portainer? (y/n): " choice
-if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
   docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
   echo "Portainer has been installed and started!"
 else
   echo "Portainer will not be installed."
 fi
+
 exit 0
