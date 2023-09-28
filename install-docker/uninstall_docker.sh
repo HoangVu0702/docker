08d4da426fa924369055c9cdcd62a6a97d420e35
hvu27 <68999625+hvu27@users.noreply.github.com>
2023-09-28 15:34:41 +0700

    Update uninstall_docker.sh

--- install-docker/uninstall_docker.sh
+++ install-docker/uninstall_docker.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
-# Check if the script is executed with root privileges
+# Kiểm tra xem script được thực thi với đặc quyền root hay không
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script with root privileges (sudo)."
+    echo "Vui lòng chạy script này với quyền root (sudo)."
     exit 1
 fi
 
-# Check if the OS argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 [Ubuntu|CentOS]"
+# Xác định hệ điều hành
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+    if [[ $ID == "ubuntu" || $ID_LIKE == "debian" ]]; then
+        OS="Ubuntu"
+    elif [[ $ID == "centos" || $ID == "rhel" || $ID == "fedora" ]]; then
+        OS="CentOS"
+    else
+        echo "Hệ điều hành không được hỗ trợ."
+        exit 1
+    fi
+else
+    echo "Không thể xác định hệ điều hành."
     exit 1
 fi
-
-OS="$1"
 
 case "$OS" in
     Ubuntu)
-        # Uninstall Docker on Ubuntu
-        echo "Uninstalling Docker on Ubuntu..."
+        # Gỡ cài đặt Docker trên Ubuntu
+        echo "Đang gỡ cài đặt Docker trên Ubuntu..."
         apt-get -y purge docker-ce docker-ce-cli containerd.io
         ;;
     CentOS)
-        # Uninstall Docker on CentOS
-        echo "Uninstalling Docker on CentOS..."
+        # Gỡ cài đặt Docker trên CentOS
+        echo "Đang gỡ cài đặt Docker trên CentOS..."
         yum -y remove docker-ce docker-ce-cli containerd.io
         ;;
     *)
-        echo "Unsupported OS. Please specify 'Ubuntu' or 'CentOS'."
+        echo "Hệ điều hành không được hỗ trợ."
         exit 1
         ;;
 esac
 
-# Remove Docker installation files and directories
-echo "Removing Docker installation files and directories..."
+# Xóa các tệp và thư mục cài đặt Docker
+echo "Đang xóa các tệp và thư mục cài đặt Docker..."
 rm -rf /var/lib/docker
 rm -rf /etc/docker
 rm -rf /etc/systemd/system/docker.service.d
 rm -rf /usr/local/bin/docker-compose
-sudo rm /etc/apt/keyrings/docker.gpg
-sudo rm /etc/apt/sources.list.d/docker.list
-# Remove Docker GPG key and APT/YUM sources
-echo "Removing Docker GPG key and package repository..."
+rm /etc/apt/keyrings/docker.gpg
+
+# Xóa khóa GPG Docker và các nguồn APT/YUM
+echo "Đang xóa khóa GPG Docker và kho lưu trữ gói..."
 if [ "$OS" == "Ubuntu" ]; then
     rm /etc/apt/sources.list.d/docker.list
     rm /etc/apt/sources.list.d/docker.list.save
@@ -49,8 +57,8 @@ elif [ "$OS" == "CentOS" ]; then
     rm /etc/yum.repos.d/docker-ce.repo
 fi
 
-# Update package lists and clean the cache
-echo "Updating package lists and cleaning the cache..."
+# Cập nhật danh sách gói và làm sạch bộ nhớ cache
+echo "Cập nhật danh sách gói và làm sạch bộ nhớ cache..."
 if [ "$OS" == "Ubuntu" ]; then
     apt-get update
     apt-get -y autoremove
@@ -59,4 +67,4 @@ elif [ "$OS" == "CentOS" ]; then
     yum -y clean all
 fi
 
-echo "Docker uninstallation and cleanup completed for $OS."
+echo "Hoàn tất việc gỡ cài đặt Docker và làm sạch cho hệ điều hành $OS."
